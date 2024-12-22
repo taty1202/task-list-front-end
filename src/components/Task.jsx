8385cc9a74ae4aace13077f681925a01fad92e1a
Tatyana Venanzi <tatyanavenanzi@gmail.com>
2024-12-22 12:47:50 -0800

    deleted console logs

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,17 +2,13 @@ import PropTypes from 'prop-types';
 import './Task.css';
 
 const Task = ({ id, title, isComplete, onToggleComplete, onDeleteTask }) => {
-  // const [complete, setComplete] = useState(isComplete);
-  // const [isRed, setIsRed] = useState(false);
   const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
-  // const colorStyle = isRed ? {color: 'red'} : {};
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
         onClick={() => {
-        // console.log('hello');
           onToggleComplete(id)}
         }>
         {title}
@@ -26,7 +22,7 @@ const Task = ({ id, title, isComplete, onToggleComplete, onDeleteTask }) => {
 Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
-  isComplete: PropTypes.bool.isOptional,
+  isComplete: PropTypes.bool.isRequired,
   onToggleComplete: PropTypes.func.isRequired,
   onDeleteTask: PropTypes.func.isRequired,
 };
